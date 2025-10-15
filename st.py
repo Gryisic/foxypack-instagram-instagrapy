803eef99c60c26e8a4d085b82f5e271e427c02d3
Dima Shapranov <shapranov.work@gmail.com>
2025-10-15 17:54:01 +0300

    del test file

--- st.py
+++
@@ -1,9 +0,0 @@
-from foxypack_instagram_instagrapy.engine import FoxyInstagramAnalysis
-
-data = FoxyInstagramAnalysis().get_analysis(
-    "https://www.instagram.com/reels/DOu9aQNjUkO/"
-)
-print(data)
-
-data = FoxyInstagramAnalysis().get_analysis("https://www.instagram.com/_fr_404/reels/")
-print(data)
